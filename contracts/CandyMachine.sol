f969d20e6d928dc8e3adb16ed66ec9d372695b16
Giannis Chatziveroglou <gchatz@mit.edu>
2022-12-26 06:07:39 +0200

    CM fixes on counters

--- contracts/CandyMachine.sol
+++ contracts/CandyMachine.sol
@@ -12,44 +12,45 @@ contract CandyMachine is ERC721URIStorage, Ownable {
     Counters.Counter private configLinesCounter;
     mapping(uint256 => string) public configLines;
 
-    uint256 supply;
-    Counters.Counter minted;
-
-    constructor(
-        string memory collectionName,
-        string memory collectionSymbol,
-        uint256 _supply
-    ) ERC721(collectionName, collectionSymbol) {
-        supply = _supply;
-    }
+    constructor(string memory collectionName, string memory collectionSymbol)
+        ERC721(collectionName, collectionSymbol)
+    {}
 
     function addConfigLine(string memory tokenUri) private {
-        require(
-            configLinesCounter.current() != supply,
-            "config lines are full for the supply"
-        );
         configLines[configLinesCounter.current()] = tokenUri;
         configLinesCounter.increment();
     }
 
+    function mintedTokensAmount() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+
+    function tokensRemainingAmount() public view returns (uint256) {
+        return configLinesCounter.current();
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current() + configLinesCounter.current();
+    }
+
     function findTokenUri() private returns (string memory) {
         string memory tokenUri = "";
-        uint256 counter = 0;
-        while (bytes(tokenUri).length == 0 || counter > supply) {
+        while (bytes(tokenUri).length == 0) {
             uint256 randomNumber = uint256(
                 keccak256(abi.encodePacked(block.difficulty, block.timestamp))
             );
-            uint256 index = randomNumber % (supply - minted.current());
+            uint256 index = randomNumber % configLinesCounter.current();
             string memory indexedData = configLines[index];
-            counter += 1;
             require(bytes(indexedData).length == 0, "invalid index");
             tokenUri = indexedData;
             delete configLines[index];
+            configLinesCounter.decrement();
         }
         return tokenUri;
     }
 
     function mintNFT(address recipient) public onlyOwner returns (uint256) {
+        require(tokensRemainingAmount() != 0, "minted out");
         _tokenIds.increment();
 
         string memory tokenUri = findTokenUri();
